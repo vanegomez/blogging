20c58081012faba2a0e3fab684433ab35885d6fb
vanegomez <vane.gomez@me.com>
2015-10-08 11:49:56 -0600

    view all posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,34 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+end
+
+
+# class IdeasController < ApplicationController
+#   def index
+#     @ideas = Idea.all
+#     @idea = Idea.new
+#   end
+#
+#   def create
+#     idea = Idea.create(idea_params)
+#
+#     render partial: "ideas/idea", locals: { idea: idea }
+#   end
+#
+#   def destroy
+#     idea = Idea.find(params[:id])
+#     idea.destroy!
+#
+#     render json: {status: :success}
+#   end
+#
+#   private
+#
+#   def idea_params
+#     params.require(:idea).permit(:title, :body)
+#   end
+# end
+
+
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true, uniqueness: true
+  validates :body, presence: true
+  validates :author, presence: true
+
+  enum status: %w(draft published)
 end
